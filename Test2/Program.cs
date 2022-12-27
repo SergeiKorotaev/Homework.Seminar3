4194ad8f7960626c7182c9b5c5a91646abc22113
Sergei Korotaev <serega_korotaev24@gmail.ru>
2022-12-27 10:27:15 +0300

    Test2

---
+++ Test2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите координаты");
+int Ax = Convert.ToInt32(Console.ReadLine());
+int Ay = Convert.ToInt32(Console.ReadLine());
+int Az = Convert.ToInt32(Console.ReadLine());
+int Bx = Convert.ToInt32(Console.ReadLine());
+int By = Convert.ToInt32(Console.ReadLine());
+int Bz = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Round(Math.Sqrt(Math.Pow(Bx - Ax, 2) + Math.Pow(By - Ay, 2) + Math.Pow(Bz - Az, 2)), 2);
+Console.WriteLine(distance);
