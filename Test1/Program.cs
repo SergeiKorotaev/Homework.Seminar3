a113086fd5dc4ab16a5346a975497a5d2642548a
Sergei Korotaev <serega_korotaev24@gmail.ru>
2022-12-27 10:14:19 +0300

    Test1

---
+++ Test1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+string num1 = num.ToString();
+if(num > 9999 || num < 100000)
+{
+    if((num.ToString()[0] == num.ToString()[4]) && (num.ToString()[1] == num.ToString()[3]))
+    {
+        Console.WriteLine("Число является полиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является полиндромом");
+    }
+}
